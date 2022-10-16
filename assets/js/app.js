f87ed59f32e321f01c3917bdb79c9e9735ec9c94
Maricel Lopez <mharycell@hotmail.com>
2022-10-16 01:01:53 -0500

    cambios ccs

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+const botonMenu = document.querySelector('.btn')
+const menu = document.querySelector('.header-nav_ul')
+
+botonMenu.addEventListener('click', () => {
+	menu.classList.toggle('mostrar-menu')
+})
